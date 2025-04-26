1045ea18fff897cfa057408b0eccd9293b7cf8d3
youjin <kujin821@naver.com>
2025-04-26 11:23:03 +0900

    feat : github 연동기능 추가

---
+++ src/main/java/com/example/gitmago/auth/github/GithubController.java
@@ -0,0 +1,13 @@
+package com.example.gitmago.auth.github;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/github")
+@RequiredArgsConstructor
+public class GithubController {
+
+}
