4ba054b7cd97b7b314e5164536ca6dd02cb38459
youjin <kujin821@naver.com>
2025-05-28 14:09:22 +0900

    fix : title model 수정

--- src/main/java/com/example/gitmago/title/Title.java
+++ src/main/java/com/example/gitmago/title/Title.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -12,6 +14,9 @@ import lombok.Setter;
 public class Title {
     private String name;
     private int level;
+    private String imageUrl;
+    private boolean obtained; //획득여부
+    private LocalDateTime obtainedAt; //언제 획득했는지
 
 }
 
