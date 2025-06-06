f3a3fbe634a6024b031707a715712ce2e910051e
youjin <kujin821@naver.com>
2025-06-07 01:56:55 +0900

    fix : git commit 수를 호출해서 받게 순환참조 관련 오류 수정

--- src/main/java/com/example/gitmago/auth/EmailController.java
+++ src/main/java/com/example/gitmago/auth/EmailController.java
@@ -26,6 +26,7 @@ public class EmailController {
             response.put("success", false);
             response.put("error", e.getMessage());
         }
+
         return ResponseEntity.ok(response);
     }
 
--- src/main/java/com/example/gitmago/title/TitleCommitService.java
+++ src/main/java/com/example/gitmago/title/TitleCommitService.java
@@ -1,21 +1,18 @@
 package com.example.gitmago.title;
 
-import com.example.gitmago.github.GithubCommitService;
+import com.example.gitmago.user.User;
 import com.example.gitmago.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.example.gitmago.user.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-
-//칭호 부여만 담당 하고 커밋 갯수 조회는 GithubCommitService 에서 불러와야됨
 @Service
 @RequiredArgsConstructor
 public class TitleCommitService {
+
     private final UserRepository userRepository;
-    private final GithubCommitService githubCommitService;
 
     // 획득한 칭호 조회
     public List<Title> getUserTitles(String username) {
@@ -60,18 +57,16 @@ public class TitleCommitService {
         }
     }
 
-    public void grantCommitTitleByCount(User user) {
-        int count = githubCommitService.getCommitCountSince(user.getGithubUsername(), user.getExpireAt());
+    //깃커밋 수 받아서 호출하게 변경
+    public void grantCommitTitleByCount(User user, int count) {
         user.setPublicCommitCount(count);
 
         if (count >= 1000) grantTitleIfNotExists(user, "찐 개발자.", 5, TitleType.COMMIT);
         else if (count >= 500) grantTitleIfNotExists(user, "개발자의 자질이 보인다", 4, TitleType.COMMIT);
-        else if (count >= 300) grantTitleIfNotExists(user, "300번째 커밋이네", 3,TitleType.COMMIT);
-        else if (count >= 100) grantTitleIfNotExists(user, "100번 찍어 안넘어가는 커밋없다", 2,TitleType.COMMIT);
-        else if (count >= 10) grantTitleIfNotExists(user, "이제 무르 익었네요.", 1,TitleType.COMMIT);
+        else if (count >= 300) grantTitleIfNotExists(user, "300번째 커밋이네", 3, TitleType.COMMIT);
+        else if (count >= 100) grantTitleIfNotExists(user, "100번 찍어 안넘어가는 커밋없다", 2, TitleType.COMMIT);
+        else if (count >= 10) grantTitleIfNotExists(user, "이제 무르 익었네요.", 1, TitleType.COMMIT);
 
-        userRepository.save(user); //레포지토리에 저장. DB 반영
+        userRepository.save(user);
     }
-
-
 }
