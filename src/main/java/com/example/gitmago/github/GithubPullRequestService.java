ad26e153009d3ed1d317abc62e95af0620a58d7c
youjin <kujin821@naver.com>
2025-06-10 14:59:36 +0900

    perf : Github Service에서 github 사용자 관련 데이터를 callback 하고, 나머지 클래스에선 데이터 정제만 처리

--- src/main/java/com/example/gitmago/github/GithubPullRequestService.java
+++ src/main/java/com/example/gitmago/github/GithubPullRequestService.java
@@ -2,11 +2,13 @@ package com.example.gitmago.github;
 
 import com.example.gitmago.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -28,25 +30,25 @@ public class GithubPullRequestService {
             }
         """.formatted(username, fromDate);
 
-        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
 
         Map<String, String> body = new HashMap<>();
         body.put("query", graphqlQuery);
 
-        var headers = new org.springframework.http.HttpHeaders();
-        headers.set("Authorization", "Bearer " + token);
-        headers.set("Accept", "application/json");
-
-        var entity = new org.springframework.http.HttpEntity<>(body, headers);
+        HttpEntity<Map<String, String>> entity = new HttpEntity<>(body, headers);
+        RestTemplate restTemplate = new RestTemplate();
 
         try {
-            var response = restTemplate.postForEntity(GITHUB_GRAPHQL_API, entity, Map.class);
+            ResponseEntity<Map> response = restTemplate.exchange(GITHUB_GRAPHQL_API, HttpMethod.POST, entity, Map.class);
             Map<?, ?> data = (Map<?, ?>) response.getBody().get("data");
             Map<?, ?> search = (Map<?, ?>) data.get("search");
             return (int) search.get("issueCount");
         } catch (Exception e) {
             e.printStackTrace();
-            return -1;
+            return -1; // 예외처리
         }
     }
 }
